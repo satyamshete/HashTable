04489a1931c62635ac7710320edf84821bd76782
satyamshete <shetesatyam99@gmail.com>
2022-08-29 19:00:12 +0530

    [Satyam] UC1-Create Hash Table And Check Frequency Of Words

---
+++ HashTable/LinkedHashMap.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class LinkedHashMap<K, V> where K : IComparable
+    {
+        private readonly int numOfBuckets;
+
+        private readonly List<LinkedList<K, V>> BucketList;
+
+        public LinkedHashMap(int numofbuckets)
+        {
+            this.numOfBuckets = numofbuckets;
+
+            BucketList = new List<LinkedList<K, V>>(numofbuckets);
+
+            for (int i = 0; i < numOfBuckets; i++)
+                BucketList.Add(null);
+        }
+
+
+        public V Get(K key)
+        {
+            int index = GetBucketIndex(key);
+
+
+            LinkedList<K, V> myLinkedList = BucketList[index];
+            if (myLinkedList == null)
+                return default;
+
+            MyMapNode<K, V> myMapNode = myLinkedList.Search(key);
+            return (myMapNode == null) ? default : myMapNode.value;
+
+
+        }
+
+        private int GetBucketIndex(K key)
+        {
+            int hashcode = Math.Abs(key.GetHashCode());
+            int index = hashcode % numOfBuckets;
+            return index;
+        }
+
+        public void Add(K key, V value)
+        {
+            int index = this.GetBucketIndex(key);
+            LinkedList<K, V> myLinkedList = this.BucketList[index];
+
+            if (myLinkedList == null)
+            {
+                myLinkedList = new LinkedList<K, V>();
+                BucketList[index] = myLinkedList;
+            }
+
+            MyMapNode<K, V> myMapNode = myLinkedList.Search(key);
+            if (myMapNode == null)
+            {
+                myMapNode = new MyMapNode<K, V>(key, value);
+                myLinkedList.Append(myMapNode);
+            }
+
+            else
+                myMapNode.value = value;
+        }
+
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HashTable");
+            LinkedHashMap<string, int> hashMap = new LinkedHashMap<string, int>(5);
+            string sentence = "to be or not to be";
+
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (var word in words)
+            {
+                int value = hashMap.Get(word);
+                if (value == default)
+                    value = 1;
+                else
+                    value += 1;
+                hashMap.Add(word, value);
+            }
+
+            int wordFrequency = hashMap.Get("to");
+            Console.WriteLine("Frequency of the word 'to' is: "+wordFrequency);
+        }
+    }
+}
